4e7bb15acca91d059eddb45b6eac3cfbf95d44ec
Yoichi Isagi <46914944+yoichisagi@users.noreply.github.com>
2026-07-15 13:03:31 +0600

    home: make git user/email conditional and add safer defaults (prepare agenix/age workflow)

--- home/git.nix
+++ home/git.nix
@@ -1,18 +1,41 @@
 { config, pkgs, ... }:
+
+let
+  lib = pkgs.lib;
+  secretPath = "${config.home.homeDirectory}/.config/nixpkgs/git-secrets.nix";
+  hasSecrets = builtins.pathExists secretPath;
+  secrets = if hasSecrets then import secretPath else {};
+in
 {
   programs.git = {
     enable = true;
     lfs.enable = true;
 
-    settings = {
-      user = {
-        name = "komi7";
-        email = "korea.ashik@gmail.com";
+    settings = (if hasSecrets then { user = secrets.user; } else {}) // {
+      core = {
+        editor = "nvim";
+        pager = "delta";
+        excludesfile = "${config.home.homeDirectory}/.gitignore_global";
       };
 
-      core.editor = "nvim";
-      pull.rebase = true;
-      init.defaultBranch = "main";
+      pull = { rebase = true; };
+      init = { defaultBranch = "main"; };
+
+      color = { ui = true; };
+      push = { default = "simple"; };
+      credential = { helper = "cache --timeout=3600"; };
+
+      commit = { gpgSign = true; };
+      gpg = { program = "gpg"; };
+
+      alias = {
+        co = "checkout";
+        br = "branch";
+        ci = "commit";
+        st = "status";
+        unstage = "reset HEAD --";
+        last = "log -1 --stat";
+      };
     };
   };
 
@@ -20,4 +43,14 @@
     enable = true;
     enableGitIntegration = true;
   };
+
+  home.file.".gitignore_global".text = ''
+    .DS_Store
+    *.swp
+    .idea/
+    .vscode/
+    .env
+    node_modules/
+    dist/
+  '';
 }
